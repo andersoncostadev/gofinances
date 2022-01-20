ee9751359e960baea5bdb0086c84f6841afbe03a
Anderson Costa <anderson.costa@meliuz.com.br>
2022-01-20 15:35:22 -0300

    style: Adiciona estilo do componente Dashboard

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #999;
+`;
+
+export const Title = styled.Text``;
