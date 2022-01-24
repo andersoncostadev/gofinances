47053404ce8b41f11f9bff03705ff177b1f965e4
Anderson Costa <anderson.costa@meliuz.com.br>
2022-01-24 10:02:00 -0300

    chore: Adiciona  Cabeçario na tela register

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Container, Header, Title} from './styles';
+
+const Register: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+    </Container>
+  );
+};
+
+export default Register;
