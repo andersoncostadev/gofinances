d2eac423e51fb2aededc7e1a3ab8d144f7212171
Anderson Costa <anderson.costa@meliuz.com.br>
2022-01-27 17:32:06 -0300

    style: Adiciona estilos na screen Register

--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -1,7 +1,11 @@
 import {RFValue} from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  background-color: ${({theme}) => theme.colors.background};
+
+  flex: 1;
+`;
 
 export const Header = styled.View`
   background-color: ${({theme}) => theme.colors.primary};
@@ -17,4 +21,19 @@ export const Title = styled.Text`
   font-family: ${({theme}) => theme.fonts.regular};
   font-size: ${RFValue(18)}px;
   color: ${({theme}) => theme.colors.shape};
+`;
+
+export const Form = styled.View`
+  flex: 1;
+  justify-content: space-between;
+  width: 100%;
+
+  padding: 24px;
+`;
+
+export const Fields = styled.View``;
+
+export const TransactionsTypes = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
 `;
